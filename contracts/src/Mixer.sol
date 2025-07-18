5ab8419b7e7e1fae4aebbb225b354a0771159672
SuyashAlphaC <iitjeesuyash2023@gmail.com>
2025-07-18 11:19:41 +0530

    Using IncrementalMerkleTree

--- contracts/src/Mixer.sol
+++ contracts/src/Mixer.sol
@@ -1,21 +1,28 @@
 //SPDX-License-Identifier:MIT
 
 pragma solidity ^0.8.19;
-
-contract Mixer {
+import {IncrementalMerkleTree, Poseidon2} from "./IncrementalMerkleTree.sol";
+contract Mixer is IncrementalMerkleTree {
     IVerifier public immutable i_verifier;
 
     mapping(bytes32 => bool) public s_commitments;
     uint256 public constant DENOMINATION = 0.001 ether;
+
+    mapping(bytes32 =>bool) public s_nullifierHashes;
+    
+    event Deposit(bytes32 commitment);
+
     
 
     error Mixer__InvalidDenomination(uint256 ethDeposited, uint256 denomination);
-    error Mixer__CommitmentAlreadySaved(bytes32 commitment);
-    constructor(IVerifier _verifier) {
+    error Mixer__UnknownRoot(bytes32 _root);
+    error Mixer__NullifierAlreadyUsed(bytes32 _nullifierHash);
+    error Mixer__CommitmentAlreadySaved(bytes32 commitment, uint32 index, uint256 timestamp);
+    constructor(IVerifier _verifier, uint32 _treeDepth,  Poseidon2 _hasher)  IncrementalMerkleTree(_treeDepth, _hasher) {
         i_verifier = _verifier;
     }
 
-    function deposit(bytes32 _commitment) public payable{
+    function deposit(bytes32 _commitment) external payable{
         if(s_commitments[_commitment]) {
             revert Mixer__CommitmentAlreadySaved(_commitment);
         }
@@ -23,7 +30,21 @@ contract Mixer {
         if(msg.value != DENOMINATION) {
             revert Mixer__InvalidEthDeposition(msg.value, DENOMINATION);
         }
-
+        uint32 inserttedIndex = _insert(commitment);
         s_commitments[_commitment] = true;
+
+        emit Deposit(commitment,insertedIndex, block.timestamp);
+    }
+
+    function withdraw(bytes32 proof , bytes32 _root , bytes32 _nullifierHash) external {
+        if(s_root != _root) {
+            revert Mixer__UnknownRoot(_root);
+        }
+
+        if(s_nullifierHashes[_nullifierHash]) {
+            revert Mixer__NullifierAlreadyUsed(_nullifierHash);
+        }
+
+        s_nullifierHashes[_nullifierHash] = true;
     }
 }
